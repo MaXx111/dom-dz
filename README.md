08a9fff70a02840a1dc7ff82a268336e8b6719c3
Sidorov Maksim <maxx11167@gmail.com>
2023-02-25 17:27:56 +0300

    add readme and appveyor

---
+++ README.md
@@ -0,0 +1,3 @@
+#DOM
+
+![CI](https://github.com/MaXx111/env/actions/workflows/web.yml/badge.svg)
--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,3 @@ const random = Math.floor(Math.random() * (items.length - 1));
 img.src = './image/goblin.png';
 img.classList.add('goblin');
 items[random].insertBefore(img, null);
-
-// <img class="goblin" src="./image/goblin.png" alt="">
