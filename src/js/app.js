e571aa623c2072bb366fa1091d379def30ce5fa5
Sidorov Maksim <maxx11167@gmail.com>
2023-02-25 17:22:43 +0300

    fix for lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,9 @@
-const item = document.querySelector('.square');
-const table = document.querySelector('.table');
+const items = document.querySelectorAll('.square');
 
-const img = document.createElement('img')
-
+const img = document.createElement('img');
+const random = Math.floor(Math.random() * (items.length - 1));
 img.src = './image/goblin.png';
 img.classList.add('goblin');
-item.insertBefore(img, null);
+items[random].insertBefore(img, null);
 
-//<img class="goblin" src="./image/goblin.png" alt="">
+// <img class="goblin" src="./image/goblin.png" alt="">
